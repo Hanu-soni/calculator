3f72a6f2bd7bfdc4445ac999f78ce48ae6f70aac
unknown <Hanu-soni>
2023-12-01 13:46:55 +0530

    small css changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import Operator from './components/Operator.jsx';
 import './css/Calculator.css';
 
-function App() {
+function App( id="calculator") {
   const [result, setResult] = useState('');
 
   const handleOperatorClick = (operator) => {
     // Handle operator click and update the result state
+    if(operator=='C'){
+      setResult("");
+      return;
+    }
     setResult((prevResult) => prevResult + operator);
   };
 
@@ -28,16 +32,24 @@ function App() {
 
   return (
     <div className="calculator">
-      <h1 className="center text-bold color-primary">Welcome to Calculator</h1>
-      <div className="calculator-box">
-        <div className="calculator-top">{result}</div>
-        <div className="calculator-bottom">
-          {['*', '/', '%', '+', '-', '1', '2', '3', '4', '5', '6', '7', '8', '='].map((item, index) => (
-            <Operator key={index} item={item} onClick={item === '=' ? handleEqualsClick : handleOperatorClick} />
-          ))}
-        </div>
-      </div>
+  <h1 className="center text-bold color-primary">Welcome to Calculator</h1>
+  <div className="calculator-box">
+    <div className={result.charAt(result.length - 1) === '+' ? "calculator-top-operator" : "calculator-top"}>
+      {result}
     </div>
+    <div className="calculator-bottom">
+      {['*', '/', '%', '+', '-','0', '1', '2', '3', '4', '5', '6', '7', '8','9', '=', "C", "(", ")"].map((item, index) => (
+        <Operator
+          key={index}
+          item={item}
+          onClick={item === '=' ? handleEqualsClick : handleOperatorClick}
+          className={isNaN(Number(item)) ? "operator-button" : "number-button"} // Apply styles based on the type
+        />
+      ))}
+    </div>
+  </div>
+</div>
+
   );
 }
 
